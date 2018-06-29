8f91a9b66401f1b443939d5d03592bcf5d06fedd
Mary Alice Orr <m.a.orr17@gmail.com>
2018-06-28 19:26:59 -0500

    finished setup and added base components

---
+++ src/components/Animals/Animals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Animals.css';
+
+class Animals extends React.Component {
+  render () {
+    return (
+      <div className="Listing">
+        <h2>Animals</h2>
+      </div>
+    );
+  }
+}
+
+export default Animals;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Form.css';
+
+class Form extends React.Component {
+  render () {
+    return (
+      <div className="Form">
+        <h2>Form</h2>
+      </div>
+    );
+  }
+}
+
+export default Form;
